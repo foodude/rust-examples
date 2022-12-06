84603e96a0a067d39221064882a4f53072691eff
coro <dev@c0r0.org>
2022-12-06 20:31:56 +0100

    add ascii-clock

---
+++ ascii-clock/src/main.rs
@@ -0,0 +1,31 @@
+use chrono::{Local};
+
+const DIGITS : [[&str; 11]; 4] = [ 
+    [" ___ ", " ___ ", " ___ ", " ___ ", " ___ ", " ___ ", " ___ ", " ___ ", " ___ ", " ___ ", " "],
+    ["|   |", "/_  |", "|_  |", "|_  |", "| | |", "|  _|", "|  _|", "|_  /", "| . |", "| . |", "*"],
+    ["| | |", "  | |", "|  _|", "|_  |", "|_  |", "|_  |", "| . |", " / / ", "| . |", "|_  |", " "],
+    ["|___|", "  |_|", "|___|", "|___|", "  |_|", "|___|", "|___|", "/_/  ", "|___|", "|___|", "*"],
+];
+
+fn main() {
+    print!("\x1b[2J");
+    print!("\x1b[?25l");
+    loop {
+        let t = Local::now();
+        let time = t.format("%H:%M:%S").to_string();
+        for row in &DIGITS {
+            for c in time.chars() {
+                let col = match c { 
+                    '0'..='9' => c as usize - '0' as usize,
+                    ':' => 10, 
+                    _ => 10, 
+                };
+                print!("{} ", row[col]);
+            }
+            println!();
+        }
+        std::thread::sleep(std::time::Duration::from_millis(999));
+        print!("\x1b[4A");
+    }   
+}
+
